3a29d089c7bd1a2322398ed8df4e45257c9385cf
Paul Strecke <145996925+Paul-Strecke@users.noreply.github.com>
2025-10-17 10:31:26 +0200

    created run method in control-plane

--- crates/control-plane/src/lib.rs
+++ crates/control-plane/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use data_plane::Sum;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn run(){
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    let sum:Sum = Sum::new(1,2);
+
+    println!("{}", Sum::compute(&sum));
 }
