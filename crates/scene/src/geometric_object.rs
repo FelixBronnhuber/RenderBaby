8f76fa7c5c5ce1ac154bebec0845a4b9d7553ca5
Michael Dollinger <mdollinger@C-5CG3200VMQ>
2025-11-01 13:57:08 +0100

    added: impl for Camera and Circle. changed visibilities

--- crates/scene/src/geometric_object.rs
+++ crates/scene/src/geometric_object.rs
@@ -1,24 +1,64 @@
-use glam::Vec3;
+use glam::{Vec3};
 
-struct GeometricObject{
+pub struct GeometricObject{
     //one or more triangles
     triangles: Option<Vec<Triangle>>,
     circle: Option<Circle>
 }
-struct Circle {
+pub struct Circle {
+    center: Vec3,
     radius: f32,
     material: Material
 }
-struct Triangle{
+impl Circle {
+    pub fn scale(&mut self, factor: f32) -> f32 {
+        self.radius = self.radius * factor;
+        self.radius
+    }
+
+    pub fn set_radius(&mut self, radius: f32) {
+        self.radius = radius;
+    }
+
+    pub fn translate(&mut self, vec: Vec3) -> Vec3 {
+        self.center = self.center + vec;
+        self.center
+    }
+
+    pub fn get_material(&self) -> &Material {
+        &self.material
+    }
+    pub fn set_material(&mut self, material: Material) {
+        self.material = material;
+    }
+    pub fn rotate(&mut self) {}
+
+} 
+    
+pub struct Triangle{
     points: Vec<Vec3>,
     material: Material
 }
-struct Camera{
+pub struct Camera{
     position: Vec3,
     rotation: Rotation
 }
+impl Camera{
+    pub fn set_position(&mut self, position: Vec3) {
+        self.position = position;
+    }
+    pub fn set_rotation(&mut self, pitch: f32, yaw: f32){
+        self.rotation = Rotation { pitch, yaw }
+    }
+    pub fn position(&self) -> Vec3 {
+        self.position
+    }
+    pub fn rotation(&self) -> &Rotation {
+        &self.rotation
+    }
+}
 
-struct Rotation{
+pub(crate) struct Rotation{
     pitch: f32,
     yaw: f32,
 }
@@ -29,13 +69,13 @@ impl Rotation{
     }
     fn get_rotation(&self) -> (f32, f32) {(self.pitch,self.yaw)}
 }
-struct LightSource{
+pub struct LightSource{
     position: Vec3
 }
 
-struct Material{
+pub struct Material{
 
 }
-struct Color{
+pub struct Color{
 
 }
