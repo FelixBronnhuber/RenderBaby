f129f67fecd012872e8e9bc76bf98534b5ae1dee
Michael Dollinger <mdollinger@C-5CG3200VMQ>
2025-11-02 17:59:26 +0100

    added: primitive test

--- crates/scene/src/lib.rs
+++ crates/scene/src/lib.rs
@@ -1,12 +1,20 @@
 mod scene;
 mod geometric_object;
+mod scene_graph;
 
 #[cfg(test)]
 mod tests {
+    use glam::Vec3;
+
+    use crate::{geometric_object::{Material, Sphere}, scene::Scene};
+
     use super::*;
 
     #[test]
     fn it_works() {
         assert_eq!(4, 4);
+        let sphere = Sphere::new(Vec3::new(0.0,0.0,0.0), 1.0, Material{});
+        assert_eq!(sphere.get_radius(), 1.0);
+        let scene = Scene::new();
     }
 }
