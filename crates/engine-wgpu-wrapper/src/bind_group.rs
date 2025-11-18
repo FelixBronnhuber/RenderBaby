3235b5a5b4811ac3037ffad51b12e325ca063fea
Felix Bronnhuber <bronfe01@thu.de>
2025-11-18 12:18:49 +0100

    Engine crate refactoring (#109)
    * Integrated prototype files from engine wgpu pair-programming
    
    * Added READMEs for new crates
    
    * Added dummy function to satisfy cargo fmt for empty placeholder lib.rs
    
    * Fixed clippy errors - neccessary after activating ci for sprint branches
    
    * Added sprint-* as a branch target in ci action (#63)
    
    * Added sprint-* as a branch target in ci action
    
    * Integrated prototype files from engine wgpu pair-programming
    
    * Added READMEs for new crates
    
    * Added dummy function to satisfy cargo fmt for empty placeholder lib.rs
    
    * Fixed clippy errors - neccessary after activating ci for sprint branches
    
    ---------
    
    Co-authored-by: Felix Bronnhuber
    
    * Added interactive FOV
    
    In order to do so:
    - had to extract engine data types and config into engine-config crate
    - added engine-main crate for later refactoring of structure
    - added fov slider to main example
    - updated render interface to pass RenderCommand from engine-config
    
    * Also updating and passing through the spheres buffer
    
    * fixed cli block temporarily
    
    * PBI: windowed app (#69)
    
    * Added last stable eframe dependency
    
    * start empty window in main
    
    * A
    start empty window in main
    
    * Created a GUI in application loop
    
    * Added additional comments to ease the understanding of code and further work with it
    
    * Deleted old code which is now stored in branch 'proto-save-pbi-windowed-app'.
    
    * Added basic MVC structure and simple functions. This version only works as an eframe version for now.
    
    * Fixed merge problem of dependencies in Cargo.toml
    
    * added set_image() and display_image() to display an image via pixelbuffer
    
    * Ran cargo fmt to pass CI
    
    * Added ignores for clippy. This is generally not good but ok here for the initial code.
    
    * Fix formatting in Cargo.toml dependencies list
    
    ---------
    
    Co-authored-by: unknown <siggto01@thu.de>
    Co-authored-by: Paul Strecke <145996925+Paul-Strecke@users.noreply.github.com>
    
    * WIP. Refactoring: Builder Pattern for RenderCommand
    
    * WIP Wrapper Refactoring
    
    * WIP Engine Refactor - works but needs further work
    
    * rc is now looped through, added opt resizing (still buggy)
    
    * added buggy resizing
    
    * Crate Refactor Complete
    
    - moved Shader to seperate crate
    - added engine-core for shared objects of ray-/pathtracer
    - moved pipeline management to wrapper
    - moved shared pixel read to wrapper
    - readded selection enum for engine
    - fixed resizing bug
    
    * fixed ci errors
    
    * Removed and re-integerated code from engine-core crate
    
    * deleted shader crate, moved shader path config from config to ray-/pathtracer
    
    * moved RenderEngine Type to Engine-main
    
    * Replaced [f32; 3] instances with newtype Vec3 in engine-config crate
    
    * Switched RenderConfigBuilder to late validation in build() for ease of use
    
    * Ran cargo fmt --all
    
    * Trying to fix #110
    
    * fixed ci errors
    
    ---------
    
    Co-authored-by: Felix Bronnhuber <felix.bronnhuber@nokia.com>
    Co-authored-by: sven-hue <ziegsv01@thu.de>
    Co-authored-by: rafaelfranke <147392436+kontra01@users.noreply.github.com>
    Co-authored-by: unknown <siggto01@thu.de>
    Co-authored-by: Paul Strecke <145996925+Paul-Strecke@users.noreply.github.com>
    Co-authored-by: Sven <sven.ziegler@nokia.com>

---
+++ crates/engine-wgpu-wrapper/src/bind_group.rs
@@ -0,0 +1,81 @@
+use crate::GpuBuffers;
+
+pub struct BindGroupLayout {
+    pub bind_group_layout: wgpu::BindGroupLayout,
+}
+
+impl BindGroupLayout {
+    pub fn new(device: &wgpu::Device) -> Self {
+        let layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("Main Bind Group Layout"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: false },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Storage { read_only: true },
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
+        Self {
+            bind_group_layout: layout,
+        }
+    }
+}
+
+pub struct BindGroup {
+    pub bind_group: wgpu::BindGroup,
+}
+
+impl BindGroup {
+    pub fn new(
+        device: &wgpu::Device,
+        buffers: &GpuBuffers,
+        layout: &wgpu::BindGroupLayout,
+    ) -> Self {
+        let group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Main Bind Group"),
+            layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: buffers.camera.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: buffers.output.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 2,
+                    resource: buffers.spheres.as_entire_binding(),
+                },
+            ],
+        });
+        Self { bind_group: group }
+    }
+}
