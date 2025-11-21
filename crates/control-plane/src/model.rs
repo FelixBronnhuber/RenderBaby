7d46e12ec401d9803be99700feff305993c479e1
Felix Bronnhuber <bronfe01@thu.de>
2025-11-21 17:33:59 +0100

    Fixed merge conflicts now using new builder with vertex and spheres add

--- crates/control-plane/src/model.rs
+++ crates/control-plane/src/model.rs
@@ -10,7 +10,19 @@ pub struct Model {
 impl Model {
     pub fn new() -> Self {
         // TODO: Get this from the Data-Plane!
-        let builder = RenderConfigBuilder::new();
+        let mut builder = RenderConfigBuilder::new();
+        builder
+            .add_sphere(Sphere::new(Vec3::new(0.0, 0.6, 4.0), 0.5, Vec3::COLOR_MAGENTA).unwrap())
+            .add_sphere(Sphere::new(Vec3::new(-0.6, 0.0, 4.0), 0.5, Vec3::COLOR_GREEN).unwrap())
+            .add_sphere(Sphere::new(Vec3::new(0.0, 0.0, 4.0), 0.5, Vec3::COLOR_RED).unwrap())
+            .add_sphere(Sphere::new(Vec3::new(0.6, 0.0, 4.0), 0.5, Vec3::COLOR_BLUE).unwrap())
+            .add_sphere(Sphere::new(Vec3::new(0.0, -0.6, 4.0), 0.5, Vec3::COLOR_CYAN).unwrap())
+            .add_vertex(0.0, 0.0, 0.0) // Bottom-left
+            .add_vertex(1.0, 0.0, 0.0) // Bottom-right
+            .add_vertex(1.0, 1.0, 0.0) // Top-right
+            .add_vertex(0.0, 1.0, 0.0) // Top-left
+            .add_triangle(0, 1, 2) // First triangle
+            .add_triangle(2, 3, 1); // Second triangle
 
         let rc = builder.camera(Camera::default()).build().unwrap();
         Self {
@@ -20,13 +32,7 @@ impl Model {
 
     pub fn generate_render_output(&mut self, fov: f32, width: u32, height: u32) -> RenderOutput {
         // TODO: Get this from the Data-Plane!
-        let new_camera = Camera {
-            fov,
-            width,
-            height,
-            // width: (fov as u32 * 400).clamp(128, 2046),
-            // height: (fov as u32 * 400).clamp(128, 2046),
-        };
+        let new_camera = Camera { fov, width, height };
         let mut builder = RenderConfigBuilder::new();
         builder
             .add_sphere(Sphere::new(Vec3::new(0.0, 0.6, 4.0), 0.5, Vec3::COLOR_MAGENTA).unwrap())
