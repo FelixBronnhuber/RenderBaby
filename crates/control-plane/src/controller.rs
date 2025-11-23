cb54e37bc14f4c306954472a7a6c84d3bec3aebd
Michael Dollinger <mdollinger@C-5CG3200VMQ>
2025-11-23 16:05:27 +0100

    changed: cargo fmt

--- crates/control-plane/src/controller.rs
+++ crates/control-plane/src/controller.rs
@@ -17,9 +17,7 @@ impl ViewListener for Controller {
     fn handle_event(&mut self, event: Event) {
         match event {
             Event::DoRender => {
-                let output = self.model.generate_render_output(
-                    self.pipeline.get_fov(),
-                );
+                let output = self.model.generate_render_output(self.pipeline.get_fov());
                 if output.validate().is_ok() {
                     *self.pipeline.render_output_ppl.lock().unwrap() = Some(output);
                 }
