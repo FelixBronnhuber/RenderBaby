1c4383018fe3dcaa700b737fbd746942b05c7d06
IbacJa <ibacja01@thu.de>
2025-12-06 21:15:41 +0100

    added own very basic obj parser

--- src/data_plane/scene_io/obj_parser.rs
+++ src/data_plane/scene_io/obj_parser.rs
@@ -2,128 +2,106 @@ use std::fs;
 use std::path::Path;
 
 #[derive(Debug)]
+pub struct FaceLine {
+    pub v: Vec<u32>,
+    pub vt: Vec<u32>,
+    pub vn: Vec<u32>,
+    pub material_name: String,
+}
+#[derive(Debug)]
 #[allow(dead_code)]
 pub struct OBJParser {
-    pub vertices: Vec<f32>, //points
-    pub faces: Vec<u32>,    //faces[0] has id face_material_id[0] with the name material_id_name[0]
-    pub face_material_id: Option<Vec<usize>>,
+    pub vertices: Vec<f32>,                   //v
+    pub face_indexes: Vec<FaceLine>,          //f
     pub normals: Option<Vec<f32>>,            //vn
     pub texture_coordinate: Option<Vec<f32>>, //vt
     pub material_path: Option<Vec<String>>,
-    pub material_id_name: Option<Vec<String>>, //material_id_name of face
 }
 impl OBJParser {
     #[allow(dead_code)]
     pub fn parse(path: &str) -> Option<OBJParser> {
         let path = Path::new(path);
         let data = fs::read_to_string(path).unwrap_or_default();
-
+        if data.is_empty() {
+            return None;
+        }
         let lineiter = data.lines();
 
-        let mut v_vec: Vec<String> = Vec::with_capacity(30);
-        let mut v_slicearr: Vec<Vec<&str>> = Vec::with_capacity(30);
         let mut v_numarr = Vec::with_capacity(30);
 
-        let mut vn_vec: Vec<String> = Vec::with_capacity(30);
-        let mut vn_slicearr: Vec<Vec<&str>> = Vec::with_capacity(30);
         let mut vn_numarr = Vec::with_capacity(30);
 
-        let mut vt_vec: Vec<String> = Vec::with_capacity(30);
-        let mut vt_slicearr: Vec<Vec<&str>> = Vec::with_capacity(30);
         let mut vt_numarr = Vec::with_capacity(30);
 
-        let mut f_vec: Vec<String> = Vec::with_capacity(30);
-        let mut f_id = Vec::with_capacity(30);
-        let mut f_slicearr: Vec<Vec<&str>> = Vec::with_capacity(30);
-        let mut f_numarr = Vec::with_capacity(30);
-
+        let mut facearr = Vec::with_capacity(100);
         let mut mtl_path: Vec<String> = Vec::with_capacity(2);
 
-        let mut material_id_name = Vec::with_capacity(1);
-        material_id_name.push("NoMaterial".to_string());
-        let mut id = 0;
-
+        let mut currentmaterial = String::new();
         lineiter.for_each(|l| {
+            match l.split_once(" ") {
+                Some(("vn", vn)) => {
+                    let vec = vn.split_whitespace().collect::<Vec<&str>>();
+                    vn_numarr.push(vec[0].parse::<f32>().unwrap_or_default());
+                    vn_numarr.push(vec[1].parse::<f32>().unwrap_or_default());
+                    vn_numarr.push(vec[2].parse::<f32>().unwrap_or_default());
+                }
+                Some(("vt", vt)) => {
+                    let vec = vt.split_whitespace().collect::<Vec<&str>>();
+                    vt_numarr.push(vec[0].parse::<f32>().unwrap_or_default());
+                    vt_numarr.push(vec[1].parse::<f32>().unwrap_or_default());
+                }
+                Some(("v", v)) => {
+                    let vec = v.split_whitespace().collect::<Vec<&str>>();
+                    vec.iter()
+                        .for_each(|a| v_numarr.push(a.parse::<(f32)>().unwrap_or_default()));
+                }
+                Some(("f", f)) => {
+                    let f = f.trim();
+                    let mut face = FaceLine {
+                        v: Vec::with_capacity(50),
+                        vt: Vec::with_capacity(10),
+                        vn: Vec::with_capacity(10),
+                        material_name: String::new(),
+                    };
+                    let mut com = f.split_whitespace().collect::<Vec<&str>>();
+                    com.iter().for_each(|a| {
+                        let mut split = a.split(|x| x == '/' || x == ' ');
 
-            if l.starts_with("v ") {
-                v_vec.push(l.to_string());
-            } else if l.starts_with("f") {
-                f_vec.push(l.to_string());
-                f_id.push(id);
-            } else if l.starts_with("mtllib") {
-                let mut a = l.split_whitespace().collect::<Vec<&str>>();
-                a.remove(0);
-                mtl_path = a.iter().map(|a| a.to_string()).collect();
-            } else if l.starts_with("usemtl") {
-                material_id_name.push(l.to_string().replace("usemtl", "").trim().to_owned());
-                id += 1
-            } else if l.starts_with("vn") {
-                vn_vec.push(l.to_string());
-            } else if l.starts_with("vt") {
-                vt_vec.push(l.to_string());
+                        let first = split.next().unwrap_or_default(); //v
+                        let second = split.next().unwrap_or_default(); //vt
+                        let third = split.next().unwrap_or_default(); //vn
+
+                        face.v.push(first.parse::<u32>().unwrap());
+                        face.vt.push(second.parse::<u32>().unwrap_or_default());
+                        face.vn.push(third.parse::<u32>().unwrap_or_default());
+                        loop {
+                            let first = split.next().unwrap_or_default();
+                            if first == "" {
+                                break;
+                            }
+                            let second = split.next().unwrap_or_default(); //vt
+                            let third = split.next().unwrap_or_default(); //vn
+
+                            face.v.push(first.parse::<u32>().unwrap());
+                            face.vt.push(second.parse::<u32>().unwrap());
+                            face.vn.push(third.parse::<u32>().unwrap());
+                        }
+                    });
+                    face.material_name = currentmaterial.clone();
+                    facearr.push(face);
+                }
+                Some(("usemtl", usemtl)) => {
+                    currentmaterial = usemtl.trim().to_string();
+                }
+                Some(("mtllib", mtllib)) => mtl_path.push(mtllib.trim().to_string()),
+
+                _ => {}
             }
-        });
-
-        for s in &mut v_vec {
-            s.remove(0);
-            let str = s.trim();
-            let str = str.split_whitespace().collect::<Vec<&str>>();
-            v_slicearr.push(str);
-        }
-
-        v_slicearr.iter().for_each(|a| {
-            a.iter()
-                .for_each(|s| v_numarr.push(s.parse::<f32>().unwrap()))
-        });
-
-        for s in &mut vn_vec {
-            s.remove(0);
-            s.remove(0);
-            let str = s.trim();
-            let str = str.split_whitespace().collect::<Vec<&str>>();
-            vn_slicearr.push(str);
-        }
-
-        vn_slicearr.iter().for_each(|a| {
-            a.iter()
-                .for_each(|s| vn_numarr.push(s.parse::<f32>().unwrap()))
-        });
-
-        for s in &mut vt_vec {
-            s.remove(0);
-            s.remove(0);
-            let str = s.trim();
-            let str = str.split_whitespace().collect::<Vec<&str>>();
-            vt_slicearr.push(str);
-        }
-
-        vt_slicearr.iter().for_each(|a| {
-            a.iter()
-                .for_each(|s| vt_numarr.push(s.parse::<f32>().unwrap()))
-        });
-
-        for s in &mut f_vec {
-            s.remove(0);
-            let str = s.trim();
-            let mut str = str.split_whitespace().collect::<Vec<&str>>();
-            if !str.is_empty() && str.first().unwrap().contains("/") {
-                str = str
-                    .iter()
-                    .map(|line| line.split_once("/").map(|(str, _)| str).unwrap())
-                    .collect();
-            }
-
-            f_slicearr.push(str);
-        }
-
-        f_slicearr.iter().for_each(|a| {
-            a.iter()
-                .for_each(|s| f_numarr.push(s.parse::<u32>().unwrap()))
         });
 
         Some(OBJParser {
             vertices: v_numarr,
-            faces: f_numarr,
+            face_indexes: facearr,
             material_path: if !mtl_path.is_empty() {
                 Some(mtl_path)
             } else {
@@ -134,14 +112,8 @@ impl OBJParser {
             } else {
                 None
             },
-            face_material_id: if !f_id.is_empty() { Some(f_id) } else { None },
             texture_coordinate: if !vt_numarr.is_empty() {
                 Some(vt_numarr)
-            } else {
-                None
-            },
-            material_id_name: if !material_id_name.is_empty() {
-                Some(material_id_name)
             } else {
                 None
             },
