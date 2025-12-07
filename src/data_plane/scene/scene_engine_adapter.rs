04acb60888ef50148e62ecffaabc8384545f2e81
Michael Dollinger <mdollinger@C-5CG3200VMQ>
2025-12-07 15:19:33 +0100

    removed: temporary logging2

--- src/data_plane/scene/scene_engine_adapter.rs
+++ src/data_plane/scene/scene_engine_adapter.rs
@@ -131,9 +131,6 @@ impl Scene {
         //! Result of either the RenderOutput or a error
         info!("{self}: Render has been called. Collecting render parameters");
 
-        let s: String = serde_json::to_string(&self).unwrap();
-        info!("{}", s);
-
         let render_spheres = self.get_render_spheres();
         let render_tris = self.get_render_tris();
 
