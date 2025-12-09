2ca1166b98baccbacc01f139b6ba2ad8754bae22
Jannis Ibach <Ibacja01@thu.de>
2025-12-09 08:36:36 +0100

    168 implement own .obj parser (#228)
    * implemented own obj parser
    * implemented own mtl parser
    
    * fixed problem where an obj would not be loaded, when an mtl was listed in the obj file, but the mtl file does not exist. It will then just ignore mtl.

--- src/data_plane/scene/render_scene.rs
+++ src/data_plane/scene/render_scene.rs
@@ -1,7 +1,7 @@
 use anyhow::Error;
 use engine_config::{RenderConfigBuilder, Uniforms, RenderOutput};
 use glam::Vec3;
-use log::{info, warn, error};
+use log::{info, error};
 use scene_objects::{
     camera::{Camera, Resolution},
     light_source::{LightSource, LightType},
@@ -10,15 +10,17 @@ use scene_objects::{
     sphere::Sphere,
     tri_geometry::TriGeometry,
 };
+use scene_objects::tri_geometry::Triangle;
 use serde::Serialize;
 
 use crate::{
     compute_plane::{engine::Engine, render_engine::RenderEngine},
     data_plane::{
         scene::scene_graph::SceneGraph,
-        scene_io::{obj_parser::parseobj, scene_parser::parse_scene, img_export::export_img_png},
+        scene_io::{obj_parser::OBJParser, scene_parser::parse_scene, img_export::export_img_png},
     },
 };
+use crate::data_plane::scene_io::mtl_parser;
 use crate::data_plane::scene_io::scene_parser::SceneParseError;
 
 /// The scene holds all relevant objects, lightsources, camera
@@ -48,31 +50,86 @@ impl Scene {
         info!("Scene: Loading new scene from {path}");
         parse_scene(path)
     }
-    pub fn load_object_from_file(&mut self, path: String) -> Result<Vec<TriGeometry>, Error> {
+    pub fn load_object_from_file(&mut self, path: String) -> Result<TriGeometry, Error> {
         //! Adds new object from a obj file at path
         //! ## Parameter
         //! 'path': Path to the obj file
         //! ## Returns
         //! Result of either a reference to the new object or an error
         info!("Scene {self}: Loading object from {path}");
-        let result = parseobj(path.clone());
+        let result = OBJParser::parse(path.clone());
+
         match result {
             Ok(objs) => {
-                if objs.is_empty() {
-                    let error_msg = format!("Parsing obj from {path} returned 0 geometries");
-                    warn!("{self}: {error_msg}");
-                    Err(anyhow::bail!("{error_msg}"))
-                } else {
-                    let res = objs.clone();
-                    for obj in objs {
-                        self.add_tri_geometry(obj);
+                let mut material_name_list: Vec<String> = Vec::new();
+                let mut material_list: Vec<Material> = Vec::new();
+
+                if let Some(obj) = objs.material_path {
+                    for i in obj {
+                        let parsed = mtl_parser::MTLParser::parse(i.as_str());
+                        match parsed {
+                            Ok(parsed) => parsed.iter().for_each(|mat| {
+                                material_list.push(Material::new(
+                                    mat.name.clone(),
+                                    mat.ka.iter().map(|a| *a as f64).collect(),
+                                    mat.kd.iter().map(|a| *a as f64).collect(),
+                                    mat.ks.iter().map(|a| *a as f64).collect(),
+                                    mat.ns.into(),
+                                    mat.d.into(),
+                                ))
+                            }),
+                            Err(error) => {
+                                info!("{self}: mtl file at {i} could not be parsed");
+                            }
+                        }
                     }
-                    Ok(res)
+
+                    material_list
+                        .iter()
+                        .for_each(|mat| material_name_list.push(mat.name.clone()));
                 }
+                let mut trianglevector: Vec<Triangle> = Vec::with_capacity(100);
+                objs.faces.len();
+                for face in objs.faces {
+                    let leng = face.v.len();
+                    for i in 1..(leng - 1) {
+                        let vs = (face.v[0], face.v[i], face.v[i + 1]);
+                        let triangle = vec![
+                            Vec3::new(
+                                objs.vertices[((vs.0 - 1.0) * 3.0) as usize],
+                                objs.vertices[(((vs.0 - 1.0) * 3.0) + 1.0) as usize],
+                                objs.vertices[(((vs.0 - 1.0) * 3.0) + 2.0) as usize],
+                            ),
+                            Vec3::new(
+                                objs.vertices[((vs.1 - 1.0) * 3.0) as usize],
+                                objs.vertices[(((vs.1 - 1.0) * 3.0) + 1.0) as usize],
+                                objs.vertices[(((vs.1 - 1.0) * 3.0) + 2.0) as usize],
+                            ),
+                            Vec3::new(
+                                objs.vertices[((vs.2 - 1.0) * 3.0) as usize],
+                                objs.vertices[(((vs.2 - 1.0) * 3.0) + 1.0) as usize],
+                                objs.vertices[(((vs.2 - 1.0) * 3.0) + 2.0) as usize],
+                            ),
+                        ];
+
+                        let material_name = face.material_name.clone();
+                        if let Some(material) =
+                            material_list.iter().find(|a| a.name == material_name)
+                        {
+                            trianglevector.push(Triangle::new(triangle, Some(material.clone())));
+                        } else {
+                            trianglevector.push(Triangle::new(triangle, None));
+                        };
+                    }
+                }
+                let mut tri = TriGeometry::new(trianglevector);
+                tri.set_name(objs.name);
+                self.add_tri_geometry(tri.clone());
+                Result::Ok(tri)
             }
             Err(error) => {
                 error!("{self}: Parsing obj from {path} resulted in error: {error}");
-                Err(error)
+                Err(error.into())
             }
         }
     }
@@ -213,7 +270,7 @@ impl Scene {
         //! ## Arguments
         //! 'tri': TriGeometry that is to be added to the scene
         //!
-        info!("{self}: adding TriGeometry {:?}", tri);
+        info!("{self}: adding TriGeometry {:?}", tri.get_name());
         self.scene_graph.add_tri_geometry(tri);
     }
     pub fn add_sphere(&mut self, sphere: Sphere) {
