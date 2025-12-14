3e585547931522bbd0d90d97e31a572798b8fc13
kontra01 <kontra01@thu.de>
2025-12-14 13:43:11 +0100

    Changed pre-commit script to run clippy on all. This is just simpler

--- ci/git-hooks/pre-commit.sh
+++ ci/git-hooks/pre-commit.sh
@@ -9,18 +9,6 @@ if [ -z "$staged_files" ]; then
     exit 0
 fi
 
-staged_existing_files=""
-while IFS= read -r file; do
-    [ -f "$file" ] && staged_existing_files="$staged_existing_files$file"$'\n'
-done <<< "$staged_files"
-
-staged_existing_files=$(printf '%s' "$staged_existing_files")
-
-if [ -z "$staged_existing_files" ]; then
-    echo "[pre-commit] DONE: No existing Rust files staged for commit."
-    exit 0
-fi
-
 echo "[pre-commit] Running rustfmt on staged Rust files..."
 
 cargo fmt --all
@@ -28,13 +16,6 @@ echo "$staged_files" | xargs git add
 
 echo "[pre-commit] Running clippy (advisory, no failure)..."
 
-clippy_output=$(cargo clippy --all-targets --all-features --message-format short --color=always 2>&1 || true)
-
-# \ to / conversion for Windows paths
-normalized_clippy=$(echo "$clippy_output" | sed 's|\\|/|g' | sed 's/^/    [clippy] /')
-
-pattern=$(echo "$staged_existing_files" | paste -sd'|' -)
-
-echo "$normalized_clippy" | grep -E "$pattern" || true
+cargo clippy --all-targets --all-features --message-format short --color=always 2>&1 || true
 
 echo "[pre-commit] DONE: Finished running pre-commit hook."
