dee8f24025a5d57279e25773a6ea84727ada0b57
Michael Dollinger <mdollinger@C-5CG3200VMQ>
2025-12-15 01:01:36 +0100

    changed: Removed commented out code

--- src/data_plane/scene_proxy/proxy_scene.rs
+++ src/data_plane/scene_proxy/proxy_scene.rs
@@ -12,22 +12,12 @@ pub struct ProxyScene {
     pub camera: ProxyCamera,
     pub objects: Vec<ProxyMesh>,
     pub lights: Vec<ProxyLight>,
-    pub background_color: [f32; 3],
-    pub misc: Vec<u32>, // temp type just for ray samples, maybe replace with map
+    pub background_color: [f32; 3], // todo: change to Color
+    pub misc: Vec<u32>,             // temp type just for ray samples, maybe replace with map
 }
 // todo: ray samples are missing: have to be moved from camera to scene!
 #[allow(unused)]
 impl ProxyScene {
-    /*     pub fn new() -> Self {
-        ProxyScene {
-            objects: todo!(),
-            scene_name: todo!(),
-            camera: todo!(),
-            lights: todo!(),
-            background_color: todo!(),
-            misc: todo!(),
-        }
-    } */
     pub fn new_from_real_scene(scene: &Scene) -> Self {
         let mut objects = vec![];
         for obj in scene.get_tri_geometries() {
