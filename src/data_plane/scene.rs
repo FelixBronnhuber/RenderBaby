14eaf1ff9e1541e9ac94a7fb9771eb03ac0516f5
M-Dollinger <dollinger-michael@web.de>
2025-12-22 00:25:39 +0100

    added: basic scene test

--- src/data_plane/scene.rs
+++ src/data_plane/scene.rs
@@ -1,3 +1,4 @@
 pub mod render_scene;
 pub mod scene_engine_adapter;
 pub mod scene_graph;
+mod tests;
---
+++ src/data_plane/scene/tests.rs
@@ -0,0 +1,19 @@
+#![cfg(test)]
+use glam::Vec3;
+use scene_objects::{material::Material, sphere::Sphere};
+use crate::data_plane::scene::render_scene::Scene;
+
+#[test]
+fn basic_scene_test() {
+    let mut scene = Scene::new();
+    let s_count = 10;
+    for i in 0..s_count {
+        scene.add_sphere(Sphere::new(
+            Vec3::default(),
+            i as f32,
+            Material::default(),
+            [1.0, 0.0, 0.0],
+        ));
+    }
+    assert_eq!(scene.get_spheres().len(), s_count);
+}
