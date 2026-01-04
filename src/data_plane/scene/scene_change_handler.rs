45ea2e147216c561bf5389163879f04c6fe14b47
M-Dollinger <dollinger-michael@web.de>
2026-01-04 22:26:42 +0100

    changed: scene is in a Rc<RefCell>

--- src/data_plane/scene/scene_change_handler.rs
+++ src/data_plane/scene/scene_change_handler.rs
@@ -1,3 +1,5 @@
+use std::{cell::RefCell, rc::Rc};
+
 use anyhow::Error;
 use log::{info, warn};
 
@@ -7,13 +9,15 @@ use crate::data_plane::scene::{
 };
 
 pub(crate) struct SceneChangeHandler {
-    pub(crate) scene: Scene,
+    pub(crate) scene: Rc<RefCell<Scene>>, // maybe Arc<Mutex<scene>>
 }
 
 impl SceneChangeHandler {
-    /* pub (crate) fn new(scene: Scene) -> Self {
-        Self { scene }
-    } */
+    pub(crate) fn new(scene: Scene) -> Self {
+        Self {
+            scene: Rc::new(RefCell::new(scene)),
+        }
+    }
 
     pub(crate) fn handle_scene_change(&mut self, change: SceneChange) -> Result<(), Error> {
         //! Handles a Change in the scene, takes care of Logging and Errors...
@@ -22,11 +26,11 @@ impl SceneChangeHandler {
         match change {
             SceneChange::CameraChange(camera_change) => {
                 self.handle_camera_change(camera_change)?;
-                self.scene.update_render_config_uniform();
+                self.scene.borrow_mut().update_render_config_uniform();
             }
             SceneChange::SphereChange(sphere_change) => {
                 self.handle_sphere_change(sphere_change)?;
-                self.scene.update_render_config_spheres();
+                self.scene.borrow_mut().update_render_config_spheres();
             }
 
             SceneChange::MeshChange(mesh_change) => {
@@ -35,7 +39,7 @@ impl SceneChangeHandler {
             }
             SceneChange::LightChange(light_change) => {
                 self.handle_light_change(light_change)?;
-                self.scene.update_render_config_lights(); // maybe not needed if only rename?
+                self.scene.borrow_mut().update_render_config_lights(); // maybe not needed if only rename?
             }
         }
         Ok(())
@@ -46,58 +50,85 @@ impl SceneChangeHandler {
             CameraChange::LookAt(look_at) => {
                 info!(
                     "Change in {}: Setting  camera lookAt to {}",
-                    self.scene, look_at
+                    self.scene.borrow(),
+                    look_at
                 );
-                self.scene.get_camera_mut().set_look_at(look_at);
+                self.scene
+                    .borrow_mut()
+                    .get_camera_mut()
+                    .set_look_at(look_at);
             }
             CameraChange::Position(position) => {
                 info!(
                     "Change in {}: Setting camera position to {}",
-                    self.scene, position
+                    self.scene.borrow(),
+                    position
                 );
-                self.scene.get_camera_mut().set_position(position);
+                self.scene
+                    .borrow_mut()
+                    .get_camera_mut()
+                    .set_position(position);
             }
             CameraChange::Up(up) => {
-                info!("Change in {}: Setting camera up to {}", self.scene, up);
-                self.scene.get_camera_mut().set_up(up);
+                info!(
+                    "Change in {}: Setting camera up to {}",
+                    self.scene.borrow(),
+                    up
+                );
+                self.scene.borrow_mut().get_camera_mut().set_up(up);
             }
             CameraChange::PaneDistance(distance) => {
                 //todo err if negative, check vs res?
                 info!(
                     "Change in {}: Setting camera pane distance to {}",
-                    self.scene, distance
+                    self.scene.borrow(),
+                    distance
                 );
-                self.scene.get_camera_mut().set_pane_distance(distance);
+                self.scene
+                    .borrow_mut()
+                    .get_camera_mut()
+                    .set_pane_distance(distance);
             }
             CameraChange::PaneWidth(width) => {
                 info!(
                     "Change in {}: Setting camera pane width to {}",
-                    self.scene, width
+                    self.scene.borrow(),
+                    width
                 );
-                self.scene.get_camera_mut().set_pane_width(width);
+                self.scene
+                    .borrow_mut()
+                    .get_camera_mut()
+                    .set_pane_width(width);
             }
             CameraChange::Resolution(res) => {
                 //todo check ratio, maybe adjust pane width
                 info!(
                     "Change in {}: Setting camera resolution to {:?}",
-                    self.scene, res
+                    self.scene.borrow(),
+                    res
                 );
-                self.scene.get_camera_mut().set_resolution(res);
+                self.scene.borrow_mut().get_camera_mut().set_resolution(res);
             }
             CameraChange::RaySamples(samples) => {
                 //todo: 0 not allowed ...
                 info!(
                     "Change in {}: Setting camera ray samples to {}",
-                    self.scene, samples
+                    self.scene.borrow(),
+                    samples
                 );
-                self.scene.get_camera_mut().set_ray_samples(samples);
+                self.scene
+                    .borrow_mut()
+                    .get_camera_mut()
+                    .set_ray_samples(samples);
             }
         }
-        if self.scene.get_camera_position() == self.scene.get_camera_look_at() {
+        if self.scene.borrow_mut().get_camera_position()
+            == self.scene.borrow_mut().get_camera_look_at()
+        {
             warn!(
                 "{}: Camera position identical to camera lookAt: {}",
-                self.scene,
-                self.scene.get_camera_look_at()
+                self.scene.borrow(),
+                self.scene.borrow().get_camera_look_at()
             )
         }
         Ok(())
