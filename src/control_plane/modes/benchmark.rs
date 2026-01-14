8d2c40349b7440f994e488189fd79efee0022204
Paul Strecke <145996925+Paul-Strecke@users.noreply.github.com>
2026-01-14 18:35:57 +0100

    automated benchmark mode

--- src/control_plane/modes/benchmark.rs
+++ src/control_plane/modes/benchmark.rs
@@ -1,23 +1,48 @@
+use std::time::Instant;
+use log::{info};
 use crate::control_plane::app::App;
+use crate::data_plane::scene::render_scene::Scene;
 
-pub struct BenchmarkApp {}
+const SAMPLE_COUNTS: &[u32] = &[1, 100, 200, 1000, 2000];
+pub struct BenchmarkApp;
 
 impl BenchmarkApp {
     pub fn new() -> Self {
         Self {}
     }
+    fn benchmark(sample_count: u32) -> std::time::Duration {
+        let mut scene =
+            Scene::load_scene_from_path("fixtures/scenes/scene.json".parse().unwrap(), true)
+                .unwrap();
+        scene.render().expect("Render failed");
+        scene.get_camera_mut().set_ray_samples(sample_count);
+        scene.set_color_hash_enabled(false);
+        let start = Instant::now();
+        scene.render().expect("Render failed");
+        start.elapsed()
+    }
 }
 
 impl App for BenchmarkApp {
     fn show(self: Box<BenchmarkApp>) {
-        todo!("Do the actual implementation of running the app here")
+        let mut results: Vec<(u32, std::time::Duration)> = Vec::new();
 
-        // Orientiere dich an der cli_static.rs
-        // e.g.
-        // let scene = Scene::load_scene_from_path(PathBuf::from("some fixture"), true).unwrap();
-        // let time1 = std::time::Instant::now();
-        // let _ = scene.render();
-        // let time2 = std::time::Instant::now();
-        // println!("Render time: {:?}", time2.duration_since(time1));
+        for &samples in SAMPLE_COUNTS {
+            info!("Running render with {} samples...", samples);
+
+            let duration = Self::benchmark(samples);
+            results.push((samples, duration));
+        }
+
+        info!("----------------------------");
+        info!("Benchmark results:");
+
+        info!("{:>10} | {:>15}", "Samples", "Render Time");
+        info!("-----------+----------------");
+
+        for (samples, duration) in results {
+            info!("{:>10} | {:>15.3?}", samples, duration);
+        }
+        info!("----------------------------");
     }
 }
